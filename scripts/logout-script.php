a0a69675d52cbe319e0f2409327d1ea568af59cf
02TheBear <Fredrik.Berzins@gmail.com>
2021-03-19 17:46:54 +0100

    reformating index.php +bug fix logout Co-authored-by: Tullingemarcus <marcus@tullingeskog.se>

---
+++ scripts/logout-script.php
@@ -0,0 +1,11 @@
+<?php
+include('../config.php');
+
+//Reset OAuth access token
+$google_client->revokeToken($_SESSION['access_token']);
+
+//Destroy entire session data.
+session_destroy();
+
+//redirect page to index.php
+header('location: ../index');
