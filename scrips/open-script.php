63ff9feecbc22fe5dcdf49a516622bba8f96bbdb
02TheBear <Fredrik.Berzins@gmail.com>
2021-03-19 12:41:08 +0100

    opening scrip session check

--- scrips/open-script.php
+++ scrips/open-script.php
@@ -2,6 +2,17 @@
 // Checks i f person got sent to the script file thrue the open button
 if (isset($_POST['login-submit'])) {
 
+    .$_SESSION['user_first_name'].
+    .$_SESSION['user_last_name'].;
+    .$_SESSION['user_email_address']
+
+    $userid = $_POST['userid'];
+    $userpassword = $_POST['userpassword'];
+
+    if (empty($userid) || empty($userpassword)) {
+        header('Location: ../index.php);
+        exit();
+    }
 
 
 else  {
