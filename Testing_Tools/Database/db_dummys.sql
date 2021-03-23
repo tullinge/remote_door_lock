f8919b2bff5a9655ca202ad89afffef7cc4d5b1b
02TheBear <Fredrik.Berzins@gmail.com>
2021-03-23 23:04:17 +0100

    +Testing folder, cahges to columns database script +dummy insert script

---
+++ Testing_Tools/Database/db_dummys.sql
@@ -0,0 +1,9 @@
+INSERT INTO `RDL_admins` (`id`, `given_name`, `family_name`, `email`) VALUES 
+(NULL, 'walking', 'stick', 'walking.stick@gmail.com'),
+(NULL, 'polar', 'bear', 'polar.bear@gmail.com'),
+(NULL, 'harp', 'seal', 'harp.seal@gmail.com');
+
+INSERT INTO `RDL_log` (`id`, `given_name`, `family_name`, `email`, `time`) VALUES 
+(NULL, 'walking', 'stick', 'walking.stick@gmail.com', NULL), 
+(NULL, 'polar', 'bear', 'polar.bear@gmail.com', NULL), 
+(NULL, 'harp', 'seal', 'harp.seal@gmail.com', NULL);
