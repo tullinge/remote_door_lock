ee600f57e1ff5c1eb8c52b0acdb4a7efa8bae303
oskarblo02 <54669844+oskarblo02@users.noreply.github.com>
2021-03-24 08:13:12 +0100

    + testing scripts

---
+++ Testing_Tools/ESP32/test_wifi_connect_esp32.ino
@@ -0,0 +1,28 @@
+#include <WiFi.h> 
+
+const char* ssid     = "ssid";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "password";     // The password of the Wi-Fi network
+
+void setup() {
+  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println('\n');
+  
+  WiFi.begin(ssid, password);             // Connect to the network
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(500);
+    Serial.print('.');
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");  
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());         // Send the IP address of the ESP32 to the computer
+}
+
+void loop() { 
+  
+}
