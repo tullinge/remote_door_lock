630d8c778fb3adb633aa7a106fd57410b8824aa3
02TheBear <Fredrik.Berzins@gmail.com>
2021-03-25 13:47:49 +0100

    update to dummy script to test database, +script to add a first admin user

---
+++ Testing_Tools/Database/db_inital_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO `RDL_users` (`id`, `given_name`, `family_name`, `email`, `rank`, `added_by`, `time_added`) VALUES 
+(NULL, `Your name`, `Your lastname`, `Your email`, `3`, `added-by-terminal`, NULL)
