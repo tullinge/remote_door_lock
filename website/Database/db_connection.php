05771177ce4df1ba78fa5af378baa5ae2d9894ab
02TheBear <Fredrik.Berzins@gmail.com>
2021-03-28 13:10:40 +0200

    reformating of db_connection.php

--- website/Database/db_connection.php
+++ website/Database/db_connection.php
@@ -1,14 +1,12 @@
 <?php
-
 require 'db_info.php';
-
 $conn = mysqli_connect(
     $DATABASE_HOST,
     $DATABASE_USER,
     $DATABASE_PASSWORD,
     $DATABASE_NAME,
 );
-
-if (!$conn) {
+if (!$conn)
+{
     die('connection failed:' .mysqli_connect_error());
 }
