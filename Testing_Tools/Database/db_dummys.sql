33984d6cdfe3d9c0bdd26d70e306d0728879a6dd
02TheBear <Fredrik.Berzins@gmail.com>
2021-03-31 15:43:09 +0200

    `->' so phpmyadmin dosent complain rank 3->4

--- Testing_Tools/Database/db_dummys.sql
+++ Testing_Tools/Database/db_dummys.sql
@@ -1,9 +1,9 @@
 INSERT INTO `RDL_users` (`id`, `given_name`, `family_name`, `email`, `rank`, `added_by`, `time_added`) VALUES 
-(NULL, `walking`, `stick`, `walking.stick@gmail.com`, `1`, `dummyscript`, NULL),
-(NULL, `polar`, `bear`, `polar.bear@gmail.com`, `3`, `dummyscript`, NULL),
-(NULL, `harp`, `seal`, `harp.seal@gmail.com`, `2`, `dummyscript`, NULL);
+(NULL, 'walking', 'stick', 'walking.stick@gmail.com', '1', 'dummyscript', NULL),
+(NULL, 'polar', 'bear', 'polar.bear@gmail.com', '3', 'dummyscript', NULL),
+(NULL, 'harp', 'seal', 'harp.seal@gmail.com', '2', 'dummyscript', NULL);
 
 INSERT INTO `RDL_log` (`id`, `given_name`, `family_name`, `email`, `time`) VALUES 
-(NULL, `walking`, `stick`, `walking.stick@gmail.com`, NULL), 
-(NULL, `polar`, `bear`, `polar.bear@gmail.com`, NULL), 
-(NULL, `harp`, `seal`, `harp.seal@gmail.com`, NULL);
+(NULL, 'walking', 'stick', 'walking.stick@gmail.com', NULL), 
+(NULL, 'polar', 'bear', 'polar.bear@gmail.com', NULL), 
+(NULL, 'harp', 'seal', 'harp.seal@gmail.com', NULL);
