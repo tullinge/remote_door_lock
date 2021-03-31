56029181c40ba740a82e48a6f21e7ac4751d09eb
02TheBear <Fredrik.Berzins@gmail.com>
2021-03-31 15:49:12 +0200

    fix #13 Added feature to remove users, moderators and admins
    Co-authored-by: Tullingemarcus <marcus@tullingeskog.se>

---
+++ website/scripts/delete_user-script.php
@@ -0,0 +1,55 @@
+<?php
+include('../config.php');
+require '../database/db_connection.php';
+
+// Checks if person got sent to the script file thru the open button in the index file.
+if (isset($_POST['delete_user-submit']))
+{
+
+    $sql = 'SELECT `rank` FROM RDL_users WHERE id=?;';
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql))
+    {
+        header('Location: ../test.php?err=sqlerr1');
+        exit();
+    }
+    else
+    {
+        mysqli_stmt_bind_param($stmt, 'i', $_POST['id']);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $deleted_user_rank);
+        while (mysqli_stmt_fetch($stmt));
+
+        if (!$deleted_user_rank < $_SESSION['rank'])
+        {
+            header('Location: ../test.php?err=not-authorized'); 
+            exit();
+        }
+
+        else{
+            $sql = "DELETE FROM `RDL_users` WHERE `id` = ?;";
+            $stmt = mysqli_stmt_init($conn);
+
+            if (!mysqli_stmt_prepare($stmt, $sql))
+            {
+                header('Location: ../test.php?err=sqlerr2');
+                exit();
+            }
+            else
+            {
+                mysqli_stmt_bind_param($stmt, 'i', $_POST['id']);
+                mysqli_stmt_execute($stmt);
+                /*mysqli_stmt_store_result($stmt);*/
+                header('Location: ../test.php?sus=user-deleted');
+                exit();
+            }
+        }
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+}
+else
+{
+    header('Location: ../test.php?err=Dont-even-try');
+    exit();
+}
