e99b2507ff6a191ee590330be1ec938b4808e11e
02TheBear <Fredrik.Berzins@gmail.com>
2021-04-13 18:39:03 +0200

    allowed fallback admin to change name/email of users (fixed #33) fixed #16 to be abelt to change rank on a user by admins and fallback admin.
    Co-authored-by: Tullingemarcus <marcus@tullingeskog.se>

---
+++ website/scripts/update_user-script.php
@@ -0,0 +1,99 @@
+<?php
+//Include configuration file.
+include('../config.php');
+
+// Require database file for database connecton.
+require '../database/db_connection.php';
+
+// Checks if person got sent to the script file thru the open button in the index file.
+if (isset($_POST['update_user-submit']))
+{
+    //Sends back user if they artent properly logged in and logs them out.
+    if (empty($_SESSION['given_name']) || empty($_SESSION['family_name']) || empty($_SESSION['email']))
+    {
+        require "scripts/logout-script.php";
+        header('Location: ../index.php?err=not-loged-in-properly');
+        exit();
+    }
+    else
+    {
+        // Defining sql query and initalizing a connection to the database.
+        $sql = 'SELECT `given_name`,`family_name`,`email`,`rank` FROM RDL_users WHERE id=?;';
+        $stmt = mysqli_stmt_init($conn);
+
+        // Checking if their is a problem with the sql query.
+        if (!mysqli_stmt_prepare($stmt, $sql))
+        {
+            // Sends back user if there is a problem with sql querys sent to database.
+            header('Location: ../user_list.php?err=sqlerr1');
+            exit();
+        }
+
+        // Checks the rank of the user that is trying to remove someone.
+        else
+        {
+            // Inputs the id of the user into the query.
+            mysqli_stmt_bind_param($stmt, 'i', $_POST['id']);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_bind_result($stmt, $given_name, $family_name, $email, $rank);
+            while (mysqli_stmt_fetch($stmt));
+
+            // Checks if any of the boxes are empty. If any of the boxes are empty they will not change anything
+            if (empty($_POST['given_name']))
+            {
+                $_POST['given_name'] = $given_name;
+            }
+            else if (empty($_POST['family_name']))
+            {
+                $_POST['family_name'] = $family_name;
+            }
+            else if (empty($_POST['email']))
+            {
+                $_POST['email'] = $email;
+            }
+            else if (empty($_POST['rank']))
+            {
+                $_POST['rank'] = $rank;
+            }
+
+            // The if statment that checks so the user didn't change the html and try to delete a higher ranking person
+            if ($rank >= $_SESSION['rank'])
+            {
+                header('Location: ../user_list.php?err=not-authorized'); 
+                exit();
+            }
+
+            // Inserts the new users variables into the database.
+            else{
+                $sql = "UPDATE `RDL_users` SET `given_name`=?, `family_name`=?, `email`=?, `rank`=? WHERE `id` = ?;";
+                $stmt = mysqli_stmt_init($conn);
+
+                // Checking if there is a problem with the sql query.
+                if (!mysqli_stmt_prepare($stmt, $sql))
+                {
+                    header('Location: ../user_list.php?err=sqlerr2');
+                    exit();
+                }
+
+                // removes the user.
+                else
+                {
+                    // Inserts the variables into the query.
+                    mysqli_stmt_bind_param($stmt, 'sssii', $_POST['given_name'], $_POST['family_name'], $_POST['email'], $_POST['rank'], $_POST['id']);
+                    mysqli_stmt_execute($stmt);
+                    /*mysqli_stmt_store_result($stmt);*/
+                    header('Location: ../user_list.php?sus=user-updated');
+                    exit();
+                }
+            }
+        }
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+}
+// Sends back error message if the user didn't use the button to access the script.
+else
+{
+    header('Location: ../user_list.php?err=Dont-even-try');
+    exit();
+}
--- website/user_list.php
+++ website/user_list.php
@@ -14,7 +14,7 @@
         {
             // defines the sql query and sends it.
             $users = [];
-            $sql = "SELECT `id`,`given_name`,`family_name`,`email`,`added_by` FROM RDL_users WHERE `rank` < ".$_SESSION['rank']." ORDER BY `family_name` ASC, `given_name` ASC;";
+            $sql = "SELECT `id`,`given_name`,`family_name`,`email`,`rank`,`added_by` FROM RDL_users WHERE `rank` < ".$_SESSION['rank']." ORDER BY `family_name` ASC, `given_name` ASC;";
             $result = mysqli_query($conn, $sql);
 
             // Fetches and saves the user info in an array to be printed from.
@@ -25,17 +25,32 @@
                     "given_name" => $array[1],
                     "family_name" => $array[2],
                     "email" => $array[3],
-                    "added_by" => $array[4],
+                    "rank" => $array[4],
+                    "added_by" => $array[5],
                 ];
                 array_push($users, $user);
             }
             mysqli_close($conn);
             // Echos the list of users one user at a time.
-            foreach ($users as $user) {
+            foreach ($users as $user)
+            {
                 echo '
                     <br>'.$user['given_name'].' '.$user['family_name'].'
                     <br>'.$user['email'].'<br>
                 ';
+                if ($user['rank'] == 1)
+                {
+                    echo 'Rank: User';
+                }
+                
+                else if ($user['rank'] == 2)
+                {
+                    echo 'Rank: Moderator';
+                }
+                else if ($user['rank'] == 3)
+                {
+                    echo 'Rank: Admin';
+                }
 
                 // Checks if the person is admin or fallback admin and if they are shown the email of the person that added them.
                 if($_SESSION['rank'] == '3' || $_SESSION['rank'] == '4')
@@ -46,10 +61,71 @@
                 }
                 // Adds form and button to delete user.
                 echo '
+                    <button><p>EDIT USER</p></button>
+                    <div>
+                ';
+
+                echo '
                     <form action="scripts/delete_user-script.php" method="post">
                         <input type="hidden" name="id" value="'.$user['id'].'">
-                        <button type="submit" name="delete_user-submit"><p>REMOVE USER</p></button>
+                        <button type="submit" name="delete_user-submit">
+                            <p>REMOVE USER</p>
+                        </button>
                     </form>
+                ';
+
+                echo '
+                    <form action="scripts/update_user-script.php" method="post">
+                ';
+
+                if ($_SESSION['rank'] == '4')
+                {
+                    echo '
+                        <input type="text" value="'.$user['given_name'].'" placeholder="given_name" name="given_name">
+                        <input type="text" value="'.$user['family_name'].'" placeholder="family_name" name="family_name">
+                        <input type="text" value="'.$user['email'].'" placeholder="email" name="email">
+                    ';
+                }
+
+                if ($user['rank'] == '1')
+                {
+                    echo '<input type="radio" id="user" name="rank" value="1" checked="checked">';
+                }
+                else
+                {
+                    echo '<input type="radio" id="user" name="rank" value="1">';
+                }
+                echo '<label for="user">USER</label>';
+
+                if ($user['rank'] == '2')
+                {
+                    echo '<input type="radio" id="moderator" name="rank" value="2" checked="checked">';
+                }
+                else
+                {
+                    echo '<input type="radio" id="moderator" name="rank" value="2">';
+                }
+                echo '<label for="moderator">MODERATOR</label>';
+
+                if ($user['rank'] == '3')
+                {
+                    echo '<input type="radio" id="admin" name="rank" value="3" checked="checked">';
+                }
+                else
+                {
+                    echo '<input type="radio" id="admin" name="rank" value="3">';
+                }
+                echo '
+                        <label for="admin">ADMIN</label>
+                        <input type="hidden" name="id" value="'.$user['id'].'">
+                        <button type="submit" name="update_user-submit">
+                            <p>UPDATE USER</p>
+                        </button>
+                    </form>
+                ';
+
+                echo'
+                    </div>
                 ';
             }
 
