9523c9d01bb2b47ced6d6e1203f7ddd44fb617ca
02TheBear <Fredrik.Berzins@gmail.com>
2021-04-13 12:46:18 +0200

    added testing tool for server (check if php is working)

---
+++ Testing_Tools/server/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>RDL</title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+    </head>
+    <body>
+    <?php
+
+    require 'Database/db_connection.php';   
+
+    echo "hej";
+    ?>
+    </body>
+</html>
